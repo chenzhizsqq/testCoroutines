e8f143f7b4d0c0b49db358440f7543fd5a7cdc2f
chenzhi <chenzhi@qq.com>
2022-01-13 13:52:24 +0900

    Kotlin Coroutines Flow Catch First Each

--- app/src/main/java/com/example/testcoroutines/coroutines_flow/CoroutinesFlowActivity.kt
+++ app/src/main/java/com/example/testcoroutines/coroutines_flow/CoroutinesFlowActivity.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testcoroutines.databinding.ActivityCoroutinesFlowBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeoutOrNull
 import kotlin.system.measureTimeMillis
 
 //https://www.jianshu.com/p/fe1293e8f15c
-//Kotlin Coroutines Flow 系列
+//Kotlin Coroutines Flow 系列:是google用来替代Rxjava的协程升级。
+// 功能已经宛如Rxjava一样
 class CoroutinesFlowActivity : AppCompatActivity() {
     val TAG = "CoroutinesFlowActivity"
     private lateinit var binding: ActivityCoroutinesFlowBinding
@@ -43,6 +41,45 @@ class CoroutinesFlowActivity : AppCompatActivity() {
         }
 
 
+        binding.FlowVSSequences1.text = "Flow 不会阻塞主线程的运行"
+        binding.FlowVSSequences1.setOnClickListener {
+            FlowVSSequences1()
+        }
+
+        binding.FlowVSSequences2.text = "而 Sequences 会阻塞主线程的运行。"
+        binding.FlowVSSequences2.setOnClickListener {
+            FlowVSSequences2()
+        }
+
+        binding.tryFinally.text = "通过使用 try ... finally 实现"
+        binding.tryFinally.setOnClickListener {
+            tryFinally()
+        }
+
+        binding.onCompletionTest.text = " onCompletion 不能捕获异常，只能用于判断是否有异常。"
+        binding.onCompletionTest.setOnClickListener {
+            onCompletionTest()
+        }
+
+        binding.onCompletionCatch.text = "catch 操作符可以捕获来自上游的异常"
+        binding.onCompletionCatch.setOnClickListener {
+            onCompletionCatch()
+        }
+
+        binding.onCompletionCatchFirst.text = "catch比onCompletion更前，则 catch 操作符捕获到异常后，不会影响到下游。"
+        binding.onCompletionCatchFirst.setOnClickListener {
+            onCompletionCatchFirst()
+        }
+
+        binding.onCompletionCatchFirstEach.text = "Catch First Each，都加上了，比较齐全了。"
+        binding.onCompletionCatchFirstEach.setOnClickListener {
+            onCompletionCatchFirstEach()
+        }
+
+
+
+
+
     }
 
 
@@ -62,6 +99,14 @@ class CoroutinesFlowActivity : AppCompatActivity() {
 
         Log.e(TAG, "flowColdStreamFun: cost $time")
     }
+    /*
+    flowColdStreamFun: 1
+    flowColdStreamFun: 2
+    flowColdStreamFun: 3
+    flowColdStreamFun: 4
+    flowColdStreamFun: 5
+    flowColdStreamFun: cost 1014
+     */
 
     //channel 是 Hot Stream。而 channelFlow 实现了生产者和消费者异步非阻塞模型。
     fun channelFlowBuilder() = runBlocking {
@@ -79,6 +124,14 @@ class CoroutinesFlowActivity : AppCompatActivity() {
         }
         Log.e(TAG, "channelFlowBuilder: channelFlowBuilder $time")
     }
+    /*
+    channelFlowBuilder: 1
+    channelFlowBuilder: 2
+    channelFlowBuilder: 3
+    channelFlowBuilder: 4
+    channelFlowBuilder: 5
+    channelFlowBuilder: channelFlowBuilder 620
+     */
 
 
     //相比于 RxJava 需要使用 observeOn、subscribeOn 来切换线程，flow 会更加简单。
@@ -101,6 +154,14 @@ class CoroutinesFlowActivity : AppCompatActivity() {
 
         Log.e(TAG, "flow_builder_map: flow_builder_map $time")
     }
+    /*
+    flow_builder_map: main: 1
+    flow_builder_map: main: 4
+    flow_builder_map: main: 9
+    flow_builder_map: main: 16
+    flow_builder_map: main: 25
+    flow_builder_map: flow_builder_map 518
+     */
 
 
     //2.3 flow 取消
@@ -119,4 +180,202 @@ class CoroutinesFlowActivity : AppCompatActivity() {
 
         Log.e(TAG, "flow_cancel: Done")
     }
+    /*
+    flow_cancel: 1
+    flow_cancel: 2
+    flow_cancel: Done
+     */
+
+
+    /**
+     * Flow VS Sequences
+     * 每一个 Flow 其内部是按照顺序执行的，这一点跟 Sequences 很类似。
+    Flow 跟 Sequences 之间的区别是 Flow 不会阻塞主线程的运行，而 Sequences 会阻塞主线程的运行。
+     * */
+    fun FlowVSSequences1() = runBlocking {
+
+        launch {
+            for (j in 1..5) {
+                delay(100)
+                Log.e(TAG, "FlowVSSequences1: I'm not blocked $j")
+            }
+        }
+
+        flow {
+            for (i in 1..5) {
+                delay(100)
+                emit(i)
+            }
+        }.collect { Log.e(TAG, "FlowVSSequences1: $it") }
+
+        Log.e(TAG, "FlowVSSequences1: Done")
+    }
+    /*
+    FlowVSSequences1: 1
+    FlowVSSequences1: I'm not blocked 1
+    FlowVSSequences1: 2
+    FlowVSSequences1: I'm not blocked 2
+    FlowVSSequences1: 3
+    FlowVSSequences1: I'm not blocked 3
+    FlowVSSequences1: 4
+    FlowVSSequences1: I'm not blocked 4
+    FlowVSSequences1: 5
+    FlowVSSequences1: Done
+    FlowVSSequences1: I'm not blocked 5
+     */
+
+    //Sequences 会阻塞主线程的运行。
+    fun FlowVSSequences2() = runBlocking {
+
+        launch {
+            for (k in 1..5) {
+                delay(100)
+                Log.e(TAG, "FlowVSSequences2: I'm blocked $k")
+            }
+        }
+
+        sequence {
+            for (i in 1..5) {
+                Thread.sleep(100)
+                yield(i)
+            }
+        }.forEach { Log.e(TAG, "FlowVSSequences2: $it") }
+
+        Log.e(TAG, "FlowVSSequences2: Done")
+    }
+    /*
+    FlowVSSequences2: 1
+    FlowVSSequences2: 2
+    FlowVSSequences2: 3
+    FlowVSSequences2: 4
+    FlowVSSequences2: 5
+    FlowVSSequences2: Done
+    FlowVSSequences2: I'm blocked 1
+    FlowVSSequences2: I'm blocked 2
+    FlowVSSequences2: I'm blocked 3
+    FlowVSSequences2: I'm blocked 4
+    FlowVSSequences2: I'm blocked 5
+     */
+
+
+    //通过使用 try ... finally 实现
+    fun tryFinally() = runBlocking {
+        try {
+            flow {
+                for (i in 1..5) {
+                    delay(100)
+                    emit(i)
+                }
+            }.collect { Log.e(TAG, "tryFinally: $it") }
+        } finally {
+            Log.e(TAG, "tryFinally: Done")
+        }
+    }
+    /*
+    tryFinally: 1
+    tryFinally: 2
+    tryFinally: 3
+    tryFinally: 4
+    tryFinally: 5
+    tryFinally: Done
+     */
+
+
+    // onCompletion 不能捕获异常，只能用于判断是否有异常。
+    fun onCompletionTest() = runBlocking {
+            flow {
+                emit(1)
+                throw RuntimeException()
+            }.onCompletion { cause ->
+                if (cause != null)
+                    Log.e(TAG, "onCompletionTest: Flow completed exceptionally")
+                else
+                    Log.e(TAG, "onCompletionTest: Done")
+            }.collect { Log.e(TAG, "onCompletionTest: $it") }
+    }
+    /*
+    CoroutinesFlowActivity: onCompletionTest: 1
+    CoroutinesFlowActivity: onCompletionTest: Flow completed exceptionally
+    AndroidRuntime: FATAL EXCEPTION: main
+     */
+
+
+    //catch 操作符可以捕获来自上游的异常
+    fun onCompletionCatch() = runBlocking {
+        flow {
+            emit(1)
+            throw RuntimeException()
+        }
+            .onCompletion { cause ->
+                if (cause != null)
+                    Log.e(TAG, "onCompletionCatch: Flow completed exceptionally")
+                else
+                    Log.e(TAG, "onCompletionCatch: Done")
+            }
+            .catch{ Log.e(TAG, "onCompletionCatch: catch exception") }
+            .collect { Log.e(TAG, "onCompletionCatch: $it") }
+    }
+    /*
+    onCompletionCatch: 1
+    onCompletionCatch: Flow completed exceptionally
+    onCompletionCatch: catch exception
+     */
+
+
+    //上面的代码如果把 onCompletion、catch 交换一下位置，则 catch 操作符捕获到异常后，不会影响到下游。
+    // 因此，onCompletion 操作符不再打印"Flow completed exceptionally"
+    fun onCompletionCatchFirst() = runBlocking {
+        flow {
+            emit(1)
+            throw RuntimeException()
+        }
+            .catch{ Log.e(TAG, "onCompletionCatchFirst: catch exception") }
+            .onCompletion { cause ->
+                if (cause != null)
+                    Log.e(TAG, "onCompletionCatchFirst: Flow completed exceptionally")
+                else
+                    Log.e(TAG, "onCompletionCatchFirst: Done")
+            }
+            .collect { Log.e(TAG, "onCompletionCatchFirst: $it") }
+    }
+    /*
+    onCompletionCatchFirst: 1
+    onCompletionCatchFirst: catch exception
+    onCompletionCatchFirst: Done
+     */
+
+
+    //Catch First Each，都加上了，比较齐全了。感觉和RxJava，真的差不多了
+    fun onCompletionCatchFirstEach() = runBlocking {
+        flow {
+            for (i in 1..5) {
+                delay(100)
+                emit(i)
+                if (i==3){
+                    throw RuntimeException()
+                }
+            }
+        }
+            .onEach {
+                Log.e(TAG, "onCompletionCatchFirstEach: onEach $it" )
+            }
+            .catch{ Log.e(TAG, "onCompletionCatchFirstEach: catch exception") }
+            .onCompletion { cause ->
+                if (cause != null)
+                    Log.e(TAG, "onCompletionCatchFirstEach: Flow completed exceptionally")
+                else
+                    Log.e(TAG, "onCompletionCatchFirstEach: Done")
+            }
+            .collect { Log.e(TAG, "onCompletionCatchFirstEach: collect $it") }
+    }
+    /*
+    onCompletionCatchFirstEach: onEach 1
+    onCompletionCatchFirstEach: collect 1
+    onCompletionCatchFirstEach: onEach 2
+    onCompletionCatchFirstEach: collect 2
+    onCompletionCatchFirstEach: onEach 3
+    onCompletionCatchFirstEach: collect 3
+    onCompletionCatchFirstEach: catch exception
+    onCompletionCatchFirstEach: Done
+     */
 }
