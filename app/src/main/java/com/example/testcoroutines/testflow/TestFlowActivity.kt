e0cfff242b479d03e2b3bf51a50108297f7d1bb6
chenzhi <chenzhi@qq.com>
2022-02-03 14:10:30 +0900

    Kotlin Coroutines Flow 添加Job，协程的控制器

--- app/src/main/java/com/example/testcoroutines/testflow/TestFlowActivity.kt
+++ app/src/main/java/com/example/testcoroutines/testflow/TestFlowActivity.kt
@@ -6,6 +6,8 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.testcoroutines.databinding.ActivityTestFlowBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 import retrofit2.Retrofit
@@ -31,6 +33,7 @@ class TestFlowActivity : AppCompatActivity() {
 
         binding.flowAllPart.setOnClickListener { flowAllPart() }
         binding.flowAllPart2.setOnClickListener { flowAllPart2() }
+        binding.flowAllPartJob.setOnClickListener { flowAllPartJob() }
 
         //dataBinding mvvm
         binding.flowViewModel.setOnClickListener {
@@ -47,18 +50,16 @@ class TestFlowActivity : AppCompatActivity() {
     private fun flowViewModel() {
         runBlocking {
             flow {
-                val retrofit = Retrofit.Builder()
-                    .baseUrl("https://my-json-server.typicode.com/")
-                    .addConverterFactory(GsonConverterFactory.create()) //把json转为gson，才可以直接用LiveData.postValue
-                    .build()
-                val service = retrofit.create(TestFlowService::class.java)
-                val response = service.getFlowGson()
+                val service = testFlowService()
+                val response = service?.getFlowGson()
                 emit(response)
             }
                 .onStart { Log.e(TAG, "flowViewModel: Starting flow") }
                 .onEach {
                     Log.e(TAG, "flowViewModel: onEach : $it")
-                    viewModel.setFlowData(it)
+                    if (it != null) {
+                        viewModel.setFlowData(it)
+                    }
                 }
                 .catch { binding.error.text = it.message }
                 .onCompletion { if (it == null) Log.e(TAG, "Completed successfully") }
@@ -67,12 +68,8 @@ class TestFlowActivity : AppCompatActivity() {
     }
 
     private fun flowAllPart() = runBlocking {
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://my-json-server.typicode.com/")
-            .addConverterFactory(GsonConverterFactory.create()) //把json转为gson，才可以直接用LiveData.postValue
-            .build()
-        val service = retrofit.create(TestFlowService::class.java)
-        val response = service.getFlowGson()
+        val service = testFlowService()
+        val response = service?.getFlowGson()
         flow {
             emit(response)
         }
@@ -83,15 +80,32 @@ class TestFlowActivity : AppCompatActivity() {
             .collect()
     }
 
+    private fun flowAllPartJob() = runBlocking {
+        val service = testFlowService()
+        val response = service?.getFlowGson()
+
+        val flow: Flow<List<TestFlowData>> = flow {
+            if (response != null) {
+                emit(response)
+            }
+        }
+
+        val job = flow.launchIn(CoroutineScope(Dispatchers.IO))
+
+        flow.onStart { Log.e(TAG, "flowAllPart: Starting flow") }
+            .onEach { binding.resultsTextview.text = it.toString() }
+            .catch { binding.error.text = it.message }
+            .onCompletion { if (it == null) Log.e(TAG, "Completed successfully") }
+            .collect()
+
+        job.cancel()
+    }
+
     private fun flowAllPart2() {
         runBlocking {
             flow {
-                val retrofit = Retrofit.Builder()
-                    .baseUrl("https://my-json-server.typicode.com/")
-                    .addConverterFactory(GsonConverterFactory.create()) //把json转为gson，才可以直接用LiveData.postValue
-                    .build()
-                val service = retrofit.create(TestFlowService::class.java)
-                val response = service.getFlowGson()
+                val service = testFlowService()
+                val response = service?.getFlowGson()
                 emit(response)
             }
                 .onStart { Log.e(TAG, "flowAllPart2: Starting flow") }
@@ -100,6 +114,14 @@ class TestFlowActivity : AppCompatActivity() {
                 .onCompletion { if (it == null) Log.e(TAG, "Completed successfully") }
                 .collect()
         }
+    }
+
+    private fun testFlowService(): TestFlowService? {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://my-json-server.typicode.com/")
+            .addConverterFactory(GsonConverterFactory.create()) //把json转为gson，才可以直接用LiveData.postValue
+            .build()
+        return retrofit.create(TestFlowService::class.java)
     }
 
 
